aed9dd1dffda62cb64cf0cb1d7c407bd7dc1cedd
sklef5 <109036061+sklef5@users.noreply.github.com>
2023-03-03 10:10:17 +0000

    Deploying to gh-pages from @ sklef5/goit-js-hw-09@8d0c88606e30e224469fb9d12b831f898e4735e6 🚀

---
+++ 01-color-switcher.6c52f220.js
@@ -0,0 +1,2 @@
+let t=null;const e=document.querySelector("button[data-start]");document.querySelector("button[data-stop]").addEventListener("click",(()=>{e.removeAttribute("disabled","true"),clearInterval(t)}));const o=()=>{document.body.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16)}`};e.addEventListener("click",(function(){e.setAttribute("disabled","true"),t=setInterval(o,1e3)}));
+//# sourceMappingURL=01-color-switcher.6c52f220.js.map
