6e6b0171e32deba555bb8fa7943af31d155f5a4a
sklef5 <109036061+sklef5@users.noreply.github.com>
2023-03-03 13:14:04 +0000

    Deploying to gh-pages from @ sklef5/goit-js-hw-09@e77b9c8f676775af9687a37576205324d4d01d92 🚀

--- 01-color-switcher.9fdcd250.js
+++
@@ -1,2 +0,0 @@
-!function(){var t=null,e=document.querySelector("button[data-start]");document.querySelector("button[data-stop]").addEventListener("click",(function(){e.removeAttribute("disabled","true"),clearInterval(t)}));var n=function(){document.body.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16))};e.addEventListener("click",(function(){e.setAttribute("disabled","true"),t=setInterval(n,1e3)}))}();
-//# sourceMappingURL=01-color-switcher.9fdcd250.js.map
---
+++ 01-color-switcher.cbe81ac9.js
@@ -0,0 +1,2 @@
+let t=null;const e=document.querySelector("button[data-start]");document.querySelector("button[data-stop]").addEventListener("click",(()=>{e.disabled="false",clearInterval(t)}));const o=()=>{document.body.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16)}`};e.addEventListener("click",(()=>{e.disabled="true",t=setInterval(o,1e3)}));
+//# sourceMappingURL=01-color-switcher.cbe81ac9.js.map
